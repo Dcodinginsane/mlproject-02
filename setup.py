9c91a660f0e04824b527b9151a520f2c9a4faca0
dlodhi2001@gmail.com <dlodhi2001@gmail.com>
2024-05-06 23:08:21 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,53 @@
+# from setuptools import find_packages , setup
+# from typing import List
+
+# HYPEN_E_DOT = '-e .'
+# def get_requirements(file_path:str)-> List[str]:
+
+#     '''
+#     this function will return the list of requirements
+
+#     '''
+#     with open('file_path') as  file_obj:
+#         requirements = file_obj.readline()
+#         requirements=[req.replace("\n" ,"") for req in requirements]
+
+#         if HYPEN_E_DOT in requirements:
+#             requirements.remove(HYPEN_E_DOT)
+
+#     return requirements        
+
+# setup(
+#     name='mlproject-02',
+#     version='0.0.1' ,
+#     author='Deepak Lodhi',
+#     author_email= 'dlodhi2001@gmail.com',
+#     packages= find_packages(),
+#     install_requires = get_requirements('requirements.txt')
+
+
+# )
+
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function will return the list of requirements.
+    '''
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+        requirements = [req for req in requirements if req and req != HYPEN_E_DOT]
+    return requirements        
+
+setup(
+    name='mlproject-02',
+    version='0.0.1',
+    author='Deepak Lodhi',
+    author_email='dlodhi2001@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
